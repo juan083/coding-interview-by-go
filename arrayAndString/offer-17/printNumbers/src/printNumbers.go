b50dd2aa8ae417c330d0efb432bad0ed9e5ce57a
mengjuan <juan083@163.com>
2021-08-05 22:42:17 +0800

    剑指 Offer 17. 打印从 1 到最大的 n 位数

---
+++ arrayAndString/offer-17/printNumbers/src/printNumbers.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+/**
+思路：
+1.求最大值：n位，则每位都是9，再转化为数值类型
+2.从i=1开始递增，直到i=最大值，把期间的数值写入切片
+*/
+func printNumbers(n int) []int {
+	if n == 0 {
+		return nil
+	}
+	var maxStr string
+	for i := 1; i <= n; i++ {
+		maxStr += "9"
+	}
+	fmt.Println(maxStr)
+	max, _ := strconv.Atoi(maxStr)
+	var ret []int
+	for i := 1; i <= max; i++ {
+		ret = append(ret, i)
+	}
+	return ret
+}
+
+func main() {
+	nums := printNumbers(2)
+	fmt.Println(nums)
+}
