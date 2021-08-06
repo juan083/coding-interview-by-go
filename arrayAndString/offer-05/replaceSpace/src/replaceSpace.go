b8ee54fec2a1c23b61cc0149e4279dcdf15bc8da
mengjuan <juan083@163.com>
2021-08-06 10:18:04 +0800

    tab to space

--- arrayAndString/offer-05/replaceSpace/src/replaceSpace.go
+++ arrayAndString/offer-05/replaceSpace/src/replaceSpace.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
-	"unicode"
+    "fmt"
+    "unicode"
 )
 
 /**
@@ -12,28 +12,29 @@ import (
 
 重点：熟练掌握go的字符串替换，拼接
 */
+
 func replaceSpace(s string) string {
-	var newString []rune
-	// []rune(【字符串】)，将【字符串】转化为unicode编码的切片
-	replace := []rune("%20")
-	// v的类型是rune，即int32，unicode编码
-	for _, v := range s {
-		// 使用 unicode.IsSpace() 判断是否是空格
-		if unicode.IsSpace(v) {
-			// 拼接多个数组，需加上三个句点...
-			newString = append(newString, replace...)
-		} else {
-			newString = append(newString, v)
-		}
-	}
-	return string(newString)
+    var newString []rune
+    // []rune(【字符串】)，将【字符串】转化为unicode编码的切片
+    replace := []rune("%20")
+    // v的类型是rune，即int32，unicode编码
+    for _, v := range s {
+        // 使用 unicode.IsSpace() 判断是否是空格
+        if unicode.IsSpace(v) {
+            // 拼接多个数组，需加上三个句点...
+            newString = append(newString, replace...)
+        } else {
+            newString = append(newString, v)
+        }
+    }
+    return string(newString)
 }
 
 func main() {
-	//s := "中 国 We are happy."
-	s := "We are happy."
-	//go标准库自带的字符串替换函数 string.ReplaceAll()，但题目要求你写算法，不要用内置的函数
-	//strings.ReplaceAll(s, " ", "%20")
-	newString := replaceSpace(s)
-	fmt.Println(newString)
+    //s := "中 国 We are happy."
+    s := "We are happy."
+    //go标准库自带的字符串替换函数 string.ReplaceAll()，但题目要求你写算法，不要用内置的函数
+    //strings.ReplaceAll(s, " ", "%20")
+    newString := replaceSpace(s)
+    fmt.Println(newString)
 }
